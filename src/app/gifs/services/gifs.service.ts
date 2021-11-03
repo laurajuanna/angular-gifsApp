8b0b6a7940e58402cd802b6a3199bec95234c363
Laura Juiz <ljuiz@southernhorizon.tech>
2021-11-02 21:35:59 -0300

    controlar historial de búsqueda

--- src/app/gifs/services/gifs.service.ts
+++ src/app/gifs/services/gifs.service.ts
@@ -12,8 +12,17 @@ export class GifsService {
     return [...this._historial];
   }
 
-  buscarGifs(query: string) {
-    this._historial.unshift(query); //unshift pushea al inicio del array
+  buscarGifs(query: string = '') {
+
+    query = query.trim().toLowerCase();
+
+    // solo si no está incluido en el array lo pushea
+    if (!this._historial.includes(query)) {
+      this._historial.unshift(query); // unshift pushea al inicio del array
+      this._historial = this._historial.splice(0, 10); // solo muestra 10
+    }
+
     console.log(this._historial);
+
   }
 }
